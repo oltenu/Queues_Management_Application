629d0e293e391858909c97d67ae0cdd60fa6ca99
Darius <oldarius2002@gmail.com>
2023-04-14 23:18:09 +0300

    Implemented model

--- Queues_Management/src/main/java/model/Server.java
+++ Queues_Management/src/main/java/model/Server.java
@@ -1,8 +1,51 @@
 package model;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Server implements Runnable{
+    private BlockingQueue<Task> tasks;
+    private AtomicInteger waitingPeriod;
+    private boolean status;
+
+    public Server(){
+        tasks = new LinkedBlockingQueue<Task>();
+        waitingPeriod = new AtomicInteger(0);
+        status = false;
+    }
+
     @Override
     public void run() {
+        if(tasks.isEmpty()){
+            status = false;
+        }else{
+            status = true;
+            try{
+                wait(tasks.take().getServiceTime());
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+
+    public AtomicInteger getWaitingPeriod() {
+        return waitingPeriod;
+    }
+
+    public void setWaitingPeriod(AtomicInteger waitingPeriod) {
+        this.waitingPeriod = waitingPeriod;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
--- Queues_Management/src/main/java/model/Task.java
+++ Queues_Management/src/main/java/model/Task.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Task {
+    private int ID;
+    private int arrivalTime;
+    private int serviceTime;
+
+    public Task(int ID, int arrivalTime, int serviceTime) {
+        this.ID = ID;
+        this.arrivalTime = arrivalTime;
+        this.serviceTime = serviceTime;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setServiceTime(int serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public String toString(){
+        return "";
+    }
 }
